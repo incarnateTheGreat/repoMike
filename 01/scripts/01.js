8089b64591ba3b89b3e414c25c55beaaf4887964
Garry Tsaconas <garrytsaconas@insightactioninc.com>
2017-07-25 15:27:22 -0400

    Setting up Assignment 01.

---
+++ 01/scripts/01.js
@@ -0,0 +1,13 @@
+//Using the 'document' selectors, find the following DOM elements. Use console.log() to output your answers.
+
+/*
+1) Find the 'btn_2' ID.
+2) Find the 'btn_times' ID.
+3) Find the 'equals' class.
+4) Find the 'numeric' classes.
+
+Challenge
+---------
+5) Find a way to loop out the DOM elements for the Operators (plus, minus, times, divide).
+Hint: you can make necessary changes to the view (01.htm) in order to help you.
+*/
